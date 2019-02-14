b2ebb06893888ca4ab10d367f7c469337409ae3c
Lucas Fernando <lucasfernandoprobiotica@outlook.com>
2019-02-14 17:15:10 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Guides
+
+- [Lamp](lamp/)
+- [Core](core/)
+- [Function Reference (Stdlib)](stdlib/)
+- [Web Features](web/)
+- [Packages](packages/)
---
+++ codes/show.php
@@ -0,0 +1,6 @@
+
+<?php
+
+require_once('util.php');
+header('Content-type: application/json; charset=UTF-8');
+echo json_encode(["crons" => getTasks()]);
